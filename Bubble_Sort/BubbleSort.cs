7f67e321a27c7afa2f30d1a932577dcdf44c5cf0
amirreza <45253403+amirkhoshnejad@users.noreply.github.com>
2020-06-18 15:46:32 +0430

    add class and method

---
+++ Bubble_Sort/BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    public class BubbleSort
+    {
+        public static void BubbleSortFirst(int[] Array,int n){
+            int m,i,j;
+            for(i=n-2;i>=0;i--){
+                for(j=0;j<=i;j++){
+                    if(Array[j]>=Array[j+1]){
+                        m=Array[j];
+                        Array[j]=Array[j+1];
+                        Array[j+1]=m;
+                    }
+                }
+            }
+            Console.Write("your answer is:");
+            for(i=0;i<n;i++){
+                Console.Write(Array[i]+"/");
+            }
+        }
+        public static void BubbleSortSecond(int[] Array,int n){
+            int i,j,m,z;            
+            for(i=n-2;i>=0;i--){
+                z=0;
+                for(j=0;j<=i;j++){
+                    if(Array[j]>=Array[j+1]){
+                        m=Array[j];
+                        Array[j]=Array[j+1];
+                        Array[j+1]=m;
+                        z++;
+                    }
+                }
+                if(z==0){
+                        break;}
+            }
+            for(i=0;i<n;i++){
+                Console.Write(Array[i]+"/");
+            }
+        }
+    }
+}
--- Bubble_Sort/Program.cs
+++ Bubble_Sort/Program.cs
@@ -1,44 +1,9 @@
-using System;
+﻿using System;
 
 namespace Bubble_Sort
 {
     class Program
     {
-        static void BubbleSortFirst(int[] Array,int n){
-            int m,i,j;
-            for(i=n-2;i>=0;i--){
-                for(j=0;j<=i;j++){
-                    if(Array[j]>=Array[j+1]){
-                        m=Array[j];
-                        Array[j]=Array[j+1];
-                        Array[j+1]=m;
-                    }
-                }
-            }
-            Console.Write("your answer is:");
-            for(i=0;i<n;i++){
-                Console.Write(Array[i]+"/");
-            }
-        }
-        static void BubbleSortSecond(int[] Array,int n){
-            int i,j,m,z;            
-            for(i=n-2;i>=0;i--){
-                z=0;
-                for(j=0;j<=i;j++){
-                    if(Array[j]>=Array[j+1]){
-                        m=Array[j];
-                        Array[j]=Array[j+1];
-                        Array[j+1]=m;
-                        z++;
-                    }
-                }
-                if(z==0){
-                        break;}
-            }
-            for(i=0;i<n;i++){
-                Console.Write(Array[i]+"/");
-            }
-        }
         static void Main(string[] args){
             int n,i;
             Console.WriteLine("Please enter the number of the List:");
@@ -48,8 +13,8 @@ namespace Bubble_Sort
             for (i = 0; i < n; i++){
                 Array[i]=Convert.ToInt32(Console.ReadLine()); 
             }
-            BubbleSortFirst(Array,n);//order n^2
-            BubbleSortSecond(Array,n);//order n
+            BubbleSort.BubbleSortFirst(Array,n);//order n^2
+            BubbleSort.BubbleSortSecond(Array,n);//order n
         }
     }
 }
